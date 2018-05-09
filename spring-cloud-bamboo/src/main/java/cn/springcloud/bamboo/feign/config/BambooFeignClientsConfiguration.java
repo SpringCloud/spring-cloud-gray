1117c5dd4db99afc2257782af0c5eeddd449af50
saleson <qlichunyu@163.com>
2018-05-09 14:20:45 +0800

    bamboo feign添加读取并加载request body的控制开关

--- spring-cloud-bamboo/src/main/java/cn/springcloud/bamboo/feign/config/BambooFeignClientsConfiguration.java
+++ spring-cloud-bamboo/src/main/java/cn/springcloud/bamboo/feign/config/BambooFeignClientsConfiguration.java
@@ -1,5 +1,6 @@
 package cn.springcloud.bamboo.feign.config;
 
+import cn.springcloud.bamboo.autoconfig.properties.BambooProperties;
 import cn.springcloud.bamboo.feign.BambooFeignClient;
 import feign.Client;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,12 @@ public class BambooFeignClientsConfiguration {
 
     @Autowired
     private Client feignClient;
+    @Autowired
+    private BambooProperties bambooProperties;
 
     @Bean
     public Client bambooFeignClient() {
-        return new BambooFeignClient(feignClient);
+        return new BambooFeignClient(bambooProperties, feignClient);
     }
 
 }
