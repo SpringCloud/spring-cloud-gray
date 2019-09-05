ffc6a5507ebbf4dd059abf8df606a5c126d69e78
saleson <qlichunyu@163.com>
2019-09-05 16:13:22 +0800

    优化配置类依赖

--- spring-cloud-gray-plugins/spring-cloud-gray-plugin-event-stream/src/main/java/cn/springcloud/gray/event/GaryClientStreamEventAutoConfiguration.java
+++ spring-cloud-gray-plugins/spring-cloud-gray-plugin-event-stream/src/main/java/cn/springcloud/gray/event/GaryClientStreamEventAutoConfiguration.java
@@ -1,9 +1,11 @@
 package cn.springcloud.gray.event;
 
+import cn.springcloud.gray.GrayManager;
 import cn.springcloud.gray.client.config.GrayEventAutoConfiguration;
 import cn.springcloud.gray.event.stream.StreamInput;
 import cn.springcloud.gray.event.stream.StreamMessageListener;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -13,12 +15,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConditionalOnClass(EnableBinding.class)
 @EnableBinding({StreamInput.class})
-@ConditionalOnProperty(value = "spring.cloud.stream.bindings.GrayEventInput.destination")
+@ConditionalOnBean(GrayManager.class)
 @AutoConfigureAfter(GrayEventAutoConfiguration.class)
 public class GaryClientStreamEventAutoConfiguration {
 
 
     @Bean
+    @ConditionalOnProperty(value = "spring.cloud.stream.bindings.GrayEventInput.destination")
     public StreamMessageListener streamMessageListener(GrayEventListener grayEventListener) {
         return new StreamMessageListener(grayEventListener);
     }
