618397397fa1c70aa89d19b943351225fddb75ab
saleson <qlichunyu@163.com>
2019-09-08 20:19:19 +0800

    实现nacos插件 优化starter-gray-server spring.factories

---
+++ spring-cloud-gray-plugins/spring-cloud-gray-plugin-ribbon-nacos-discovery/src/main/java/cn/springcloud/gray/client/plugin/ribbon/nacos/NacosServerExplainer.java
@@ -0,0 +1,56 @@
+package cn.springcloud.gray.client.plugin.ribbon.nacos;
+
+import cn.springcloud.gray.servernode.ServerExplainer;
+import cn.springcloud.gray.servernode.ServerSpec;
+import com.netflix.loadbalancer.Server;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cloud.netflix.ribbon.DefaultServerIntrospector;
+import org.springframework.cloud.netflix.ribbon.ServerIntrospector;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
+
+import java.util.Map;
+
+public class NacosServerExplainer implements ServerExplainer<Server> {
+
+    private SpringClientFactory springClientFactory;
+
+    public NacosServerExplainer(SpringClientFactory springClientFactory) {
+        this.springClientFactory = springClientFactory;
+    }
+
+    @Override
+    public ServerSpec apply(Server server) {
+        String appName = server.getMetaInfo().getAppName();
+        String seviceId = appName;
+        if(StringUtils.contains(appName, "@@")){
+            seviceId = StringUtils.split(appName, "@@")[1];
+        }
+        Map metadata = getServerMetadata(seviceId, server);
+        return ServerSpec.builder().instanceId(server.getMetaInfo().getInstanceId())
+                .serviceId(seviceId)
+                .metadatas(metadata).build();
+    }
+
+    public ServerIntrospector serverIntrospector(String serviceId) {
+        if (springClientFactory == null) {
+            return new DefaultServerIntrospector();
+        }
+        ServerIntrospector serverIntrospector = this.springClientFactory.getInstance(serviceId,
+                ServerIntrospector.class);
+        if (serverIntrospector == null) {
+            serverIntrospector = new DefaultServerIntrospector();
+        }
+        return serverIntrospector;
+    }
+
+    /**
+     * 获取实例的metadata信息
+     *
+     * @param serviceId 服务id
+     * @param server    ribbon服务器(服务实例)
+     * @return 服务实例的metadata信息
+     */
+    public Map<String, String> getServerMetadata(String serviceId, Server server) {
+        return serverIntrospector(serviceId).getMetadata(server);
+    }
+}
