9cae26c2ce97ee299ae02e530b00c1b81ebee99a
saleson <qlichunyu@163.com>
2019-09-08 20:19:19 +0800

    实现nacos插件 优化starter-gray-server spring.factories

--- spring-cloud-gray-plugins/spring-cloud-gray-plugin-ribbon-nacos-discovery/src/main/java/cn/springcloud/gray/client/plugin/ribbon/nacos/configuration/GrayClientRibbonNacosAutoConfiguration.java
+++ spring-cloud-gray-plugins/spring-cloud-gray-plugin-ribbon-nacos-discovery/src/main/java/cn/springcloud/gray/client/plugin/ribbon/nacos/configuration/GrayClientRibbonNacosAutoConfiguration.java
@@ -1,14 +1,17 @@
 package cn.springcloud.gray.client.plugin.ribbon.nacos.configuration;
 
 import cn.springcloud.gray.client.config.properties.GrayHoldoutServerProperties;
+import cn.springcloud.gray.client.plugin.ribbon.nacos.NacosServerExplainer;
 import cn.springcloud.gray.client.plugin.ribbon.nacos.NacosServerListProcessor;
+import cn.springcloud.gray.servernode.ServerExplainer;
 import cn.springcloud.gray.servernode.ServerListProcessor;
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
 import com.alibaba.cloud.nacos.ribbon.NacosServerList;
-import com.alibaba.nacos.api.naming.NamingService;
 import com.netflix.loadbalancer.Server;
 import com.netflix.ribbon.Ribbon;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,8 +23,14 @@ public class GrayClientRibbonNacosAutoConfiguration {
 
     @Bean
     public ServerListProcessor<Server> serverListProcessor(
-            GrayHoldoutServerProperties grayHoldoutServerProperties, NamingService namingService){
-        return new NacosServerListProcessor(grayHoldoutServerProperties, namingService);
+            GrayHoldoutServerProperties grayHoldoutServerProperties, NacosDiscoveryProperties discoveryProperties){
+        return new NacosServerListProcessor(grayHoldoutServerProperties, discoveryProperties.namingServiceInstance());
+    }
+
+
+    @Bean
+    public ServerExplainer<Server> serverExplainer(SpringClientFactory springClientFactory){
+        return new NacosServerExplainer(springClientFactory);
     }
 
 }
