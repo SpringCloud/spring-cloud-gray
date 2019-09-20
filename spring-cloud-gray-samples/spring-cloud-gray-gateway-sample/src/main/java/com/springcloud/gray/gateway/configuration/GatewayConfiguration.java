fb4254052929860454bfe3cc85f75fef1dac7c72
saleson <qlichunyu@163.com>
2019-09-21 00:48:29 +0800

    添加限流支持

---
+++ spring-cloud-gray-samples/spring-cloud-gray-gateway-sample/src/main/java/com/springcloud/gray/gateway/configuration/GatewayConfiguration.java
@@ -0,0 +1,56 @@
+package com.springcloud.gray.gateway.configuration;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+
+//@Configuration
+public class GatewayConfiguration {
+
+
+    @Bean
+    public KeyResolver uriKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getURI().getPath());
+    }
+
+
+//    @Bean
+//    public RequestLocalStorage requestLocalStorage() {
+//        return new RequestLocalStorage() {
+//
+//            private FastThreadLocal<GrayTrackInfo> trackThreadLocal = new FastThreadLocal<>();
+//            private FastThreadLocal<GrayRequest> reqThreadLocal = new FastThreadLocal<>();
+//
+//            @Override
+//            public void setGrayTrackInfo(GrayTrackInfo grayTrackInfo) {
+//                trackThreadLocal.set(grayTrackInfo);
+//            }
+//
+//            @Override
+//            public void removeGrayTrackInfo() {
+//                trackThreadLocal.remove();
+//            }
+//
+//            @Override
+//            public GrayTrackInfo getGrayTrackInfo() {
+//                return trackThreadLocal.get();
+//            }
+//
+//            @Override
+//            public void setGrayRequest(GrayRequest grayRequest) {
+//                reqThreadLocal.set(grayRequest);
+//            }
+//
+//            @Override
+//            public void removeGrayRequest() {
+//                reqThreadLocal.remove();
+//            }
+//
+//            @Override
+//            public GrayRequest getGrayRequest() {
+//                return reqThreadLocal.get();
+//            }
+//        };
+//    }
+
+}
