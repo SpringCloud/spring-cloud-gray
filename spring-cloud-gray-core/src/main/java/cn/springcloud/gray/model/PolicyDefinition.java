9fda921c2652e239b4856180ee78a40dbb9de292
saleson <qlichunyu@163.com>
2019-09-23 21:39:37 +0800

    添加tostring

--- spring-cloud-gray-core/src/main/java/cn/springcloud/gray/model/PolicyDefinition.java
+++ spring-cloud-gray-core/src/main/java/cn/springcloud/gray/model/PolicyDefinition.java
@@ -2,9 +2,9 @@ package cn.springcloud.gray.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -12,6 +12,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 /**
  * 灰度策略，包含一组(0,n)决策元素
  */
+@ToString
 @Setter
 @Getter
 public class PolicyDefinition implements Serializable {
