79f62e6d4ff596fb48cde7ce318566e80b6c01b1
saleson <qlichunyu@163.com>
2019-10-16 12:11:11 +0800

    demo

--- spring-cloud-gray-samples/spring-cloud-gray-gateway-sample/src/main/java/com/springcloud/gray/gateway/configuration/GatewayConfiguration.java
+++ spring-cloud-gray-samples/spring-cloud-gray-gateway-sample/src/main/java/com/springcloud/gray/gateway/configuration/GatewayConfiguration.java
@@ -2,13 +2,15 @@ package com.springcloud.gray.gateway.configuration;
 
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
 
-//@Configuration
+@Configuration
 public class GatewayConfiguration {
 
 
     @Bean
+
     public KeyResolver uriKeyResolver() {
         return exchange -> Mono.just(exchange.getRequest().getURI().getPath());
     }
