3b5fd2f7042299f146fe39802f594b79ef60412c
saleson <qlichunyu@163.com>
2019-12-16 11:33:04 +0800

    。

---
+++ spring-cloud-gray-client/src/main/java/cn/springcloud/gray/refresh/RefreshDriver.java
@@ -0,0 +1,11 @@
+package cn.springcloud.gray.refresh;
+
+/**
+ * @author saleson
+ * @date 2019-12-12 15:53
+ */
+public interface RefreshDriver {
+
+    void refresh();
+
+}
---
+++ spring-cloud-gray-client/src/main/java/cn/springcloud/gray/refresh/Refresher.java
@@ -0,0 +1,11 @@
+package cn.springcloud.gray.refresh;
+
+/**
+ * @author saleson
+ * @date 2019-12-12 16:02
+ */
+public interface Refresher {
+
+    void refresh();
+
+}
