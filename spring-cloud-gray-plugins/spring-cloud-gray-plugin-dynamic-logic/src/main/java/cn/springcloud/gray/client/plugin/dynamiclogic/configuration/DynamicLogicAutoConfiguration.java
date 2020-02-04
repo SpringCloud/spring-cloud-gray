65119ac10f0cd8d554a84c725a4fef05ccaec428
saleson <qlichunyu@163.com>
2020-02-04 19:54:32 +0800

    dynamic-logic spring.factories

--- spring-cloud-gray-plugins/spring-cloud-gray-plugin-dynamic-logic/src/main/java/cn/springcloud/gray/client/plugin/dynamiclogic/configuration/DynamicLogicAutoConfiguration.java
+++ spring-cloud-gray-plugins/spring-cloud-gray-plugin-dynamic-logic/src/main/java/cn/springcloud/gray/client/plugin/dynamiclogic/configuration/DynamicLogicAutoConfiguration.java
@@ -1,8 +1,10 @@
 package cn.springcloud.gray.client.plugin.dynamiclogic.configuration;
 
+import cn.springcloud.gray.GrayManager;
 import cn.springcloud.gray.client.plugin.dynamiclogic.FmDynamicLogicDriver;
 import cn.springcloud.gray.dynamiclogic.DynamicLogicDriver;
 import com.fm.compiler.dynamicbean.DynamicBeanManager;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
  * @date 2019-12-28 11:27
  */
 @Configuration
+@ConditionalOnBean(GrayManager.class)
 public class DynamicLogicAutoConfiguration {
 
     @Bean
