7bfe71c4204ddbffe22fec141f3e0acb68db71af
saleson <qlichunyu@163.com>
2020-02-05 13:04:55 +0800

    upate spring initializer

--- spring-cloud-gray-code-component/src/main/java/cn/springlcoud/gray/code/component/initializer/SpringInitializer.java
+++ spring-cloud-gray-code-component/src/main/java/cn/springlcoud/gray/code/component/initializer/SpringInitializer.java
@@ -3,8 +3,14 @@ package cn.springlcoud.gray.code.component.initializer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author saleson
@@ -18,6 +24,25 @@ public abstract class SpringInitializer implements InitializingBean, Application
 
     protected abstract void initialization();
 
+    protected <T> T getBean(Class<T> cls) {
+        try {
+            return cxt.getBean(cls);
+        } catch (NoUniqueBeanDefinitionException e) {
+            throw e;
+        } catch (BeansException e) {
+            log.warn("没有从spring容器中找到class为'{}'的Bean", cls);
+        }
+        return null;
+    }
+
+    protected <T> T getBean(Class<T> cls, T defaultBean) {
+        T bean = getBean(cls);
+        if (Objects.isNull(bean)) {
+            return defaultBean;
+        }
+        return bean;
+    }
+
 
     protected <T> T getBean(String beanName, Class<T> cls) {
         T t = null;
@@ -27,18 +52,17 @@ public abstract class SpringInitializer implements InitializingBean, Application
             log.warn("没有从spring容器中找到name为'{}', class为'{}'的Bean", beanName, cls);
         }
         if (t == null) {
-            t = cxt.getBean(cls);
+            t = getBean(cls);
         }
         return t;
     }
 
     protected <T> T getBean(String beanName, Class<T> cls, T defaultBean) {
-        try {
-            return getBean(beanName, cls);
-        } catch (BeansException e) {
-            log.warn("没有从spring容器中找到name为'{}', class为'{}'的Bean, 返回默认的bean:{}", beanName, cls, defaultBean);
+        T bean = getBean(beanName, cls);
+        if (Objects.isNull(bean)) {
             return defaultBean;
         }
+        return bean;
     }
 
     protected <T> T getBeanNullable(String beanName, Class<T> cls) {
@@ -48,6 +72,11 @@ public abstract class SpringInitializer implements InitializingBean, Application
             log.warn("没有从spring容器中找到name为'{}', class为'{}'的Bean", beanName, cls);
             return null;
         }
+    }
+
+    protected <T> List<T> getBeans(Class<T> cls) {
+        Map<String, T> beans = cxt.getBeansOfType(cls);
+        return new ArrayList<>(beans.values());
     }
 
     @Override
