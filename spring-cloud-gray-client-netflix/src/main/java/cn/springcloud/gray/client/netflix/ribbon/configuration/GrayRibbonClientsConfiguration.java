b1873df674dc36960d346aafaf84a88d6db293bc
saleson <qlichunyu@163.com>
2020-05-28 00:32:15 +0800

    支持ribbon IRule 公平负载可配置

--- spring-cloud-gray-client-netflix/src/main/java/cn/springcloud/gray/client/netflix/ribbon/configuration/GrayRibbonClientsConfiguration.java
+++ spring-cloud-gray-client-netflix/src/main/java/cn/springcloud/gray/client/netflix/ribbon/configuration/GrayRibbonClientsConfiguration.java
@@ -1,6 +1,8 @@
 package cn.springcloud.gray.client.netflix.ribbon.configuration;
 
+import cn.springcloud.gray.IGrayChooseConfig;
 import cn.springcloud.gray.client.netflix.ribbon.GrayChooserRule;
+import cn.springcloud.gray.client.netflix.ribbon.GrayLoadBalanceFairPossibleRule;
 import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.ZoneAvoidanceRule;
@@ -11,10 +13,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GrayRibbonClientsConfiguration {
 
+    @Autowired
+    private IGrayChooseConfig grayChooseConfig;
+
     @Bean
     public IRule ribbonRule(
             @Autowired(required = false) IClientConfig config) {
-        ZoneAvoidanceRule rule = new GrayChooserRule();
+        ZoneAvoidanceRule rule = grayChooseConfig.isChooseServerFairPossible() ?
+                new GrayLoadBalanceFairPossibleRule() :
+                new GrayChooserRule();
         rule.initWithNiwsConfig(config);
         return rule;
     }
