40ee6ee0d9cd75d6394b6e5cc5311fe39aaebc38
saleson <qlichunyu@163.com>
2020-08-17 21:58:45 +0800

    gray.holdoutServer to gray.holdout-server

--- spring-cloud-gray-client/src/main/java/cn/springcloud/gray/client/config/properties/GrayHoldoutServerProperties.java
+++ spring-cloud-gray-client/src/main/java/cn/springcloud/gray/client/config/properties/GrayHoldoutServerProperties.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @Data
 @NoArgsConstructor
-@ConfigurationProperties("gray.holdoutServer")
+@ConfigurationProperties("gray.holdout-server")
 public class GrayHoldoutServerProperties {
 
     private boolean enabled;
