2951877b3f1240d5c8fe1d9025be2444e1a3ee80
saleson <qlichunyu@163.com>
2020-08-18 01:48:15 +0800

    demo

--- spring-cloud-gray-samples/spring-cloud-gray-apollo-config-sample/src/main/java/cn/springcloud/gray/apollo/sample/ApolloSampleApplication.java
+++ spring-cloud-gray-samples/spring-cloud-gray-apollo-config-sample/src/main/java/cn/springcloud/gray/apollo/sample/ApolloSampleApplication.java
@@ -2,6 +2,7 @@ package cn.springcloud.gray.apollo.sample;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -20,7 +21,7 @@ public class ApolloSampleApplication {
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(ApolloSampleApplication.class);
 
     public static void main(String[] args) throws UnknownHostException {
-        Environment env = new SpringApplicationBuilder(ApolloSampleApplication.class).web(true).run(args).getEnvironment();
+        Environment env = new SpringApplicationBuilder(ApolloSampleApplication.class).web(WebApplicationType.SERVLET).run(args).getEnvironment();
         log.info(
                 "\n----------------------------------------------------------\n\t"
                         + "Application '{}' is running! Access URLs:\n\t" + "Local: \t\thttp://127.0.0.1:{}\n\t"
--- spring-cloud-gray-samples/spring-cloud-gray-consumer-nacos-config-sample/src/main/java/cn/springcloud/gray/service/consumer/nacos/connfig/NacosConsumerApplication.java
+++ spring-cloud-gray-samples/spring-cloud-gray-consumer-nacos-config-sample/src/main/java/cn/springcloud/gray/service/consumer/nacos/connfig/NacosConsumerApplication.java
@@ -1,6 +1,7 @@
 package cn.springcloud.gray.service.consumer.nacos.connfig;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -18,7 +19,7 @@ public class NacosConsumerApplication {
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(NacosConsumerApplication.class);
 
     public static void main(String[] args) throws UnknownHostException {
-        Environment env = new SpringApplicationBuilder(NacosConsumerApplication.class).web(true).run(args).getEnvironment();
+        Environment env = new SpringApplicationBuilder(NacosConsumerApplication.class).web(WebApplicationType.SERVLET).run(args).getEnvironment();
         log.info(
                 "\n----------------------------------------------------------\n\t"
                         + "Application '{}' is running! Access URLs:\n\t" + "Local: \t\thttp://127.0.0.1:{}\n\t"
