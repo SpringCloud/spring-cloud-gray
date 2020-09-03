d94e8e94d3c534fd365f9619da4ff24be7cbbefe
saleson <qlichunyu@163.com>
2020-09-03 16:52:30 +0800

    排除冲突的jar 修复NacosServiceDiscovery无法被其它项目引用的bug

--- spring-cloud-gray-server-plugins/spring-cloud-gray-server-plugin-nacos-discovery/src/main/java/cn/springcloud/gray/server/plugin/discovery/nacos/configuration/GrayServerPluginNacosDiscoveryAutoConfiguration.java
+++ spring-cloud-gray-server-plugins/spring-cloud-gray-server-plugin-nacos-discovery/src/main/java/cn/springcloud/gray/server/plugin/discovery/nacos/configuration/GrayServerPluginNacosDiscoveryAutoConfiguration.java
@@ -1,17 +1,16 @@
 package cn.springcloud.gray.server.plugin.discovery.nacos.configuration;
 
+import cn.springcloud.gray.server.discovery.ServiceDiscovery;
 import cn.springcloud.gray.server.plugin.discovery.nacos.NacosServiceDiscovery;
 import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnClass(NacosServiceDiscovery.class)
 public class GrayServerPluginNacosDiscoveryAutoConfiguration {
 
     @Bean
-    public NacosServiceDiscovery nacosServiceDiscovery(NacosDiscoveryProperties discoveryProperties){
+    public ServiceDiscovery serviceDiscovery(NacosDiscoveryProperties discoveryProperties) {
         return new NacosServiceDiscovery(discoveryProperties);
     }
 }
