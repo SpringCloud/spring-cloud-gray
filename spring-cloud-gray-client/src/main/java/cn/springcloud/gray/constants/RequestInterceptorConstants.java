0eef71724b6af379f443923dac0b2f2ec0923d87
saleson <qlichunyu@163.com>
2020-09-29 17:56:09 +0800

    RequestInterceptorConstants迁包

---
+++ spring-cloud-gray-client/src/main/java/cn/springcloud/gray/constants/RequestInterceptorConstants.java
@@ -0,0 +1,14 @@
+package cn.springcloud.gray.constants;
+
+/**
+ * @author saleson
+ * @date 2020-09-27 15:02
+ */
+public class RequestInterceptorConstants {
+
+    public static final String INTERCEPTRO_TYPE_ALL = "all";
+    public static final String INTERCEPTRO_TYPE_FEIGN = "feign";
+    public static final String INTERCEPTRO_TYPE_ZUUL = "zuul";
+    public static final String INTERCEPTRO_TYPE_RESTTEMPLATE = "rest";
+
+}
